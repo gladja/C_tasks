9db13fbaf4fb4bc0a5186fe4910f5d81c9849ef7
gladja <user@MacBook-Pro-user.local>
2023-07-20 00:37:38 +0300

    task-05

---
+++ task-05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task_05
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] data = Console.ReadLine().Trim().Split();
+            int a = int.Parse(data[0]);
+            int b = int.Parse(data[1]);
+
+            int s = a * b;
+            int p = 2 * (a + b);
+            Console.WriteLine("The perimeter of the rectangle with sides {0} and {1} is {2}.", a, b, p);
+            Console.WriteLine("The area of the rectangle with sides {0} and {1} is {2}.", a, b, s);
+        }
+    }
+}

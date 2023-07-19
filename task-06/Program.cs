745dc6e4f2a3414d9f6488051a9cf827f3271ccc
gladja <user@MacBook-Pro-user.local>
2023-07-20 00:56:36 +0300

    task-06

---
+++ task-06/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task_01
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int nextNum = num + 1;
+            int prevNum = num - 1;
+
+            Console.WriteLine("The next number for {0} is {1}.", num, nextNum);
+            Console.WriteLine("The previous number for {0} is {1}.", num, prevNum);
+        }
+    }
+}

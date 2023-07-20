1fccaa2eef9a7e01ce02f1afce48cdd3790f55d3
gladja <user@MacBook-Pro-user.local>
2023-07-20 22:50:02 +0300

    task-14

---
+++ task-14/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task_13
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sum = 5 * (12 * (123 * (1234 + 54321) + 4321) + 12) * 5;
+            Console.WriteLine(sum);
+        }
+    }
+}

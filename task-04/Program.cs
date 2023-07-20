2f40eaa0f773fefb9b21cf40bf2731cc32ecfa00
gladja <user@MacBook-Pro-user.local>
2023-07-20 08:13:51 +0300

    task-04

---
+++ task-04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task_04
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int year = Convert.ToInt32(Console.ReadLine()) + 18;
+            Console.WriteLine("{0} will reach adulthood in {1}", name, year);
+            // Console.WriteLine(dateBirth);
+        }
+    }
+}

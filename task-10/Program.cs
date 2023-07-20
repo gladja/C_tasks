1ad0d14d788dd1a801417bf06fc6cf9427054314
gladja <user@MacBook-Pro-user.local>
2023-07-20 17:57:33 +0300

    task-10

---
+++ task-10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("program HelloWorld;\n" + "begin\n" + "  WriteLn('Hello, World!');\n" + "end.");
+        }
+    }
+}

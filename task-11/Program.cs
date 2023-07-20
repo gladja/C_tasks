1a5a557a0382dfd264e51ac3261002ee4a00b76f
gladja <user@MacBook-Pro-user.local>
2023-07-20 18:23:55 +0300

    task-11

---
+++ task-11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            double sumOne = ((4.625 - (13.0 / 18) * (9.0 / 26)) / (9.0 / 4) + 2.5 / 1.25 / 6.75) / (121.0 / 68);
+            double sumTwo = (0.5 - 0.375) / 0.125 + ((5.0 / 6) - (7.0 / 12)) / (0.358 - 1.4796 / 13.7);
+            double sum = sumOne / sumTwo;
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+

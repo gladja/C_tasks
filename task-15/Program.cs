bed79bff906d9cf3342d2ea5b4cb4436c385251f
gladja <user@MacBook-Pro-user.local>
2023-07-20 22:55:27 +0300

    task-15

---
+++ task-15/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace task_15
+{
+    class Program
+    {
+        static void Main()
+        {
+            double sum = (7.5 / 0.5 - 12.4 / 0.4) / (6.3 / 0.3 - 4.6 / 0.2);
+
+            Console.WriteLine(sum);
+        }
+    }
+}

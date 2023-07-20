70ece3dd570e1cf5905fe026f6c41b45beecc735
gladja <gladja@yandex.ru>
2023-07-20 16:28:22 +0300

    refactor task-04

--- task-04/Program.cs
+++ task-04/Program.cs
@@ -6,10 +6,17 @@ namespace task_04
     {
         static void Main()
         {
-            string name = Console.ReadLine();
-            int year = Convert.ToInt32(Console.ReadLine()) + 18;
-            Console.WriteLine("{0} will reach adulthood in {1}", name, year);
-            // Console.WriteLine(dateBirth);
+            string nameOne = Console.ReadLine();
+            int yearOne = Convert.ToInt32(Console.ReadLine()) + 18;
+            Console.WriteLine("{0} will reach adulthood in {1}", nameOne, yearOne);
+
+            string nameTwo = Console.ReadLine();
+            int yearTwo = Convert.ToInt32(Console.ReadLine()) + 18;
+            Console.WriteLine("{0} will reach adulthood in {1}", nameTwo, yearTwo);
+
+            string nameThree = Console.ReadLine();
+            int yearThree = Convert.ToInt32(Console.ReadLine()) + 18;
+            Console.WriteLine("{0} will reach adulthood in {1}", nameThree, yearThree);
         }
     }
 }

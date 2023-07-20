244aa2143491c2704845299e62c683cbd1ca9a24
gladja <gladja@yandex.ru>
2023-07-20 16:39:03 +0300

    task-07

---
+++ task-07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] data = Console.ReadLine().Trim().Split();
+            int a = int.Parse(data[0]);
+            int b = int.Parse(data[1]);
+            int sum = a + b;
+
+            Console.WriteLine($"{a}+{b}={sum}");
+        }
+    }
+}

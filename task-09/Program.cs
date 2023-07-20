ce58079f806122930fcda123ce95722184af8762
gladja <gladja@yandex.ru>
2023-07-20 17:03:35 +0300

    task-09

---
+++ task-09/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("#include <iostream>\n");
+            Console.WriteLine("int main()\n" + "{\n" + "    std::cout << \"Hello World!\" << std::endl;\n" + "}\n");
+        }
+    }
+}

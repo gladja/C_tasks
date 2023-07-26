7985bd793f2a3e5700b20545835544cb029287d7
gladja <user@MacBook-Pro-user.local>
2023-07-27 00:01:31 +0300

    task--08

---
+++ task--08/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace task_10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int a = num / 100;
+            // Console.WriteLine(a);
+
+            int b = (num / 10) % 10;
+            // Console.WriteLine(b);
+
+            int c = num % 10;
+            // Console.WriteLine(c);
+
+            if (a > b)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+            if (b > c)
+            {
+                b = b + c;
+                c = b - c;
+                b = b - c;
+            }
+            if (a > b)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+
+            Console.WriteLine("{0}{1}{2}", c, b, a);
+
+
+        }
+    }
+}

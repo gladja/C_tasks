d3eb2085c75de512989224bf5a03fafde79a2f2c
gladja <user@MacBook-Pro-user.local>
2023-07-26 18:11:17 +0300

    task--06

--- task--06/Program.cs
+++ task--06/Program.cs
@@ -6,97 +6,37 @@ namespace task_06
     {
         static void Main()
         {
-            string[] data = Console.ReadLine().Trim().Split();
-            int a = int.Parse(data[0]);
-            int b = int.Parse(data[1]);
-            int c = int.Parse(data[2]);
-            int d = int.Parse(data[3]);
-            int e = int.Parse(data[4]);
+            // string[] data = Console.ReadLine().Trim().Split();
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            int e = Convert.ToInt32(Console.ReadLine());
 
-            if (a % 2 != 0)
-            {
+            if ((a % 2 != 0) || (a > 0 && (a > 99 && a < 1000)))
                 Console.WriteLine("YES");
-            }
-            else if (a > 0)
-            {
-                Console.WriteLine("YES");
-            }
-            else if ((a > 99) && (a < 1000))
-            {
-                Console.WriteLine("YES");
-            }
             else
-            {
                 Console.WriteLine("NO");
-            }
 
-            if (b % 2 != 0)
-            {
+            if ((b % 2 != 0) || (b > 0 && (b > 99 && b < 1000)))
                 Console.WriteLine("YES");
-            }
-            else if (b > 0)
-            {
-                Console.WriteLine("YES");
-            }
-            else if ((b > 99) && (a < 1000))
-            {
-                Console.WriteLine("YES");
-            }
             else
-            {
                 Console.WriteLine("NO");
-            }
 
-            if (c % 2 != 0)
-            {
+            if ((c % 2 != 0) || (c > 0 && (c > 99 && c < 1000)))
                 Console.WriteLine("YES");
-            }
-            else if (c > 0)
-            {
-                Console.WriteLine("YES");
-            }
-            else if ((c > 99) && (a < 1000))
-            {
-                Console.WriteLine("YES");
-            }
             else
-            {
                 Console.WriteLine("NO");
-            }
 
-            if (d % 2 != 0)
-            {
+            if ((d % 2 != 0) || (d > 0 && (d > 99 && d < 1000)))
                 Console.WriteLine("YES");
-            }
-            else if (d > 0)
-            {
-                Console.WriteLine("YES");
-            }
-            else if ((d > 99) && (a < 1000))
-            {
-                Console.WriteLine("YES");
-            }
             else
-            {
                 Console.WriteLine("NO");
-            }
 
-            if (e % 2 != 0)
-            {
+            if ((e % 2 != 0) || (e > 0 && (e > 99 && e < 1000)))
                 Console.WriteLine("YES");
-            }
-            else if (e > 0)
-            {
-                Console.WriteLine("YES");
-            }
-            else if ((e > 99) && (a < 1000))
-            {
-                Console.WriteLine("YES");
-            }
             else
-            {
                 Console.WriteLine("NO");
-            }
         }
     }
 }

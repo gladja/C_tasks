1f276d8641e4435df39900c1f1436e36cc574b45
gladja <user@MacBook-Pro-user.local>
2023-07-27 22:38:59 +0300

    task--16

---
+++ task--16/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace task_16
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            int num5 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > 0)
+            {
+                int a = num1 / 100;
+                int b = (num1 / 10) % 10;
+                int c = num1 % 10;
+
+                if ((a != b) && (a != c) && (b != c))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            if (num2 > 0)
+            {
+                int a = num2 / 100;
+                int b = (num2 / 10) % 10;
+                int c = num2 % 10;
+
+                if ((a != b) && (a != c) && (b != c))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            if (num3 > 0)
+            {
+                int a = num3 / 100;
+                int b = (num3 / 10) % 10;
+                int c = num3 % 10;
+
+                if ((a != b) && (a != c) && (b != c))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            if (num4 > 0)
+            {
+                int a = num4 / 100;
+                int b = (num4 / 10) % 10;
+                int c = num4 % 10;
+
+                if ((a != b) && (a != c) && (b != c))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            if (num5 > 0)
+            {
+                int a = num5 / 100;
+                int b = (num5 / 10) % 10;
+                int c = num5 % 10;
+
+                if ((a != b) && (a != c) && (b != c))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+
+        }
+    }
+}

96d11ab49d176095e22db9a49c20aa07a5ec8cf3
gladja <user@MacBook-Pro-user.local>
2023-07-27 22:54:50 +0300

    task--17

---
+++ task--17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_17
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine(a * a);
+            }
+            else if (a == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(a - 1);
+            }
+        }
+    }
+}

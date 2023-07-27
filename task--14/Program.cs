2f8457373fd147b668d90bbb9d5b10f4c563d18c
gladja <user@MacBook-Pro-user.local>
2023-07-27 22:13:38 +0300

    task--14

---
+++ task--14/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace task_04
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            int e = Convert.ToInt32(Console.ReadLine());
+
+            if ((a % 2 != 0) && a < 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            if ((b % 2 != 0) && b < 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            if ((c % 2 != 0) && c < 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            if ((d % 2 != 0) && d < 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            if ((e % 2 != 0) && e < 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}

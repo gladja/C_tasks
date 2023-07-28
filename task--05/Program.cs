e9f05c424d95480696a756911fb01c399ba73478
gladja <user@MacBook-Pro-user.local>
2023-07-28 10:00:37 +0300

    task--05

---
+++ task--05/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace task_10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            if (num > 99 && num < 1000)
+            {
+                int a = num / 100;
+                int b = (num / 10) % 10;
+                int c = num % 10;
+
+
+                if (a == 0 || b == 0 || c == 0)
+                {
+                    sum += 1;
+
+                    if ((c == 3 || c == 6 || c == 9))
+                    {
+                        Console.WriteLine("NO");
+                    }
+                    else
+                    {
+                        Console.WriteLine("YES");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            else
+            {
+                int a = num / 1000;
+                // Console.WriteLine(a);
+                int b = (num / 100) % 10;
+                // Console.WriteLine(b);
+                int c = (num / 10) % 10;
+                // Console.WriteLine(c);
+                int d = num % 10;
+                // Console.WriteLine(d);
+
+                if (a != b && a != b && a != d && b != c && b != d && c != d)
+                {
+                    if ((d == 3 || d == 6 || d == 9))
+                    {
+                        Console.WriteLine("NO1");
+                    }
+                    else
+                    {
+                        Console.WriteLine("YES");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO2");
+                }
+            }
+        }
+    }
+}

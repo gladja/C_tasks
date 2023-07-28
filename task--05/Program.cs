26733039148bc3ff83dbbe6852576638599eeb28
gladja <gladja@MacBook-Pro-gladja.local>
2023-07-28 17:05:48 +0300

    task--05

--- task--05/Program.cs
+++ task--05/Program.cs
@@ -7,8 +7,11 @@ namespace task_10
         static void Main()
         {
             int num = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            int num5 = Convert.ToInt32(Console.ReadLine());
 
-            int sum = 0;
 
             if (num > 99 && num < 1000)
             {
@@ -16,10 +19,8 @@ namespace task_10
                 int b = (num / 10) % 10;
                 int c = num % 10;
 
-
                 if (a == 0 || b == 0 || c == 0)
                 {
-                    sum += 1;
 
                     if ((c == 3 || c == 6 || c == 9))
                     {
@@ -38,19 +39,43 @@ namespace task_10
             else
             {
                 int a = num / 1000;
-                // Console.WriteLine(a);
                 int b = (num / 100) % 10;
-                // Console.WriteLine(b);
                 int c = (num / 10) % 10;
-                // Console.WriteLine(c);
                 int d = num % 10;
-                // Console.WriteLine(d);
+                int sum = 0;
 
-                if (a != b && a != b && a != d && b != c && b != d && c != d)
+                if ((a != b && a != b && a != d && b != c && b != d && c != d) && a <= 9)
                 {
-                    if ((d == 3 || d == 6 || d == 9))
+                    sum += 1;
+                }
+
+                if ((d == 3 || d == 6 || d == 9))
+                {
+                    sum += 1;
+                }
+
+                if (sum == 1)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            if (num2 > 99 && num2 < 1000)
+            {
+                int a = num2 / 100;
+                int b = (num2 / 10) % 10;
+                int c = num2 % 10;
+
+                if (a == 0 || b == 0 || c == 0)
+                {
+
+                    if ((c == 3 || c == 6 || c == 9))
                     {
-                        Console.WriteLine("NO1");
+                        Console.WriteLine("NO");
                     }
                     else
                     {
@@ -59,7 +84,187 @@ namespace task_10
                 }
                 else
                 {
-                    Console.WriteLine("NO2");
+                    Console.WriteLine("NO");
+                }
+            }
+            else
+            {
+                int a = num2 / 1000;
+                int b = (num2 / 100) % 10;
+                int c = (num2 / 10) % 10;
+                int d = num2 % 10;
+                int sum = 0;
+
+                if ((a != b && a != b && a != d && b != c && b != d && c != d) && a <= 9)
+                {
+                    sum += 1;
+                }
+
+                if ((d == 3 || d == 6 || d == 9))
+                {
+                    sum += 1;
+                }
+
+                if (sum == 1)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            if (num3 > 99 && num3 < 1000)
+            {
+                int a = num3 / 100;
+                int b = (num3 / 10) % 10;
+                int c = num3 % 10;
+
+                if (a == 0 || b == 0 || c == 0)
+                {
+
+                    if ((c == 3 || c == 6 || c == 9))
+                    {
+                        Console.WriteLine("NO");
+                    }
+                    else
+                    {
+                        Console.WriteLine("YES");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            else
+            {
+                int a = num3 / 1000;
+                int b = (num3 / 100) % 10;
+                int c = (num3 / 10) % 10;
+                int d = num3 % 10;
+                int sum = 0;
+
+                if ((a != b && a != b && a != d && b != c && b != d && c != d) && a <= 9)
+                {
+                    sum += 1;
+                }
+
+                if ((d == 3 || d == 6 || d == 9))
+                {
+                    sum += 1;
+                }
+
+                if (sum == 1)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            if (num4 > 99 && num4 < 1000)
+            {
+                int a = num4 / 100;
+                int b = (num4 / 10) % 10;
+                int c = num4 % 10;
+
+                if (a == 0 || b == 0 || c == 0)
+                {
+
+                    if ((c == 3 || c == 6 || c == 9))
+                    {
+                        Console.WriteLine("NO");
+                    }
+                    else
+                    {
+                        Console.WriteLine("YES");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            else
+            {
+                int a = num4 / 1000;
+                int b = (num4 / 100) % 10;
+                int c = (num4 / 10) % 10;
+                int d = num4 % 10;
+                int sum = 0;
+
+                if ((a != b && a != b && a != d && b != c && b != d && c != d) && a <= 9)
+                {
+                    sum += 1;
+                }
+
+                if ((d == 3 || d == 6 || d == 9))
+                {
+                    sum += 1;
+                }
+
+                if (sum == 1)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            if (num5 > 99 && num5 < 1000)
+            {
+                int a = num5 / 100;
+                int b = (num5 / 10) % 10;
+                int c = num5 % 10;
+
+                if (a == 0 || b == 0 || c == 0)
+                {
+
+                    if ((c == 3 || c == 6 || c == 9))
+                    {
+                        Console.WriteLine("NO");
+                    }
+                    else
+                    {
+                        Console.WriteLine("YES");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            else
+            {
+                int a = num5 / 1000;
+                int b = (num5 / 100) % 10;
+                int c = (num5 / 10) % 10;
+                int d = num5 % 10;
+                int sum = 0;
+
+                if ((a != b && a != b && a != d && b != c && b != d && c != d) && a <= 9)
+                {
+                    sum += 1;
+                }
+
+                if (d == 3 || d == 6 || d == 9)
+                {
+                    sum += 1;
+                }
+
+                if (sum == 1)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
                 }
             }
         }

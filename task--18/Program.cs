a0f82db58fe9e989cd93967d915b3953c0f0da40
gladja <user@MacBook-Pro-user.local>
2023-07-29 22:52:27 +0300

    task--18

---
+++ task--18/Program.cs
@@ -0,0 +1,282 @@
+﻿using System;
+
+namespace task_02
+{
+    class Program
+    {
+        static void Main()
+        {
+            string white = "";
+            string white1 = "";
+            string white2 = "";
+            string white3 = "";
+            string white4 = "";
+            string white5 = "";
+            string white6 = "";
+            string white7 = "";
+            string white8 = "";
+            string white9 = "";
+            string white10 = "";
+            string white11 = "";
+            string white12 = "";
+            string white13 = "";
+            string white14 = "";
+            string white15 = "";
+            string white16 = "";
+            string white17 = "";
+            string white18 = "";
+            string white19 = "";
+            string white20 = "";
+            string white21 = "";
+            string white22 = "";
+            string white23 = "";
+            string white24 = "";
+            string white25 = "";
+            string white26 = "";
+            string white27 = "";
+            string white28 = "";
+            string white29 = "";
+            string white30 = "";
+            string white31 = "";
+            string white32 = "";
+            string white33 = "";
+            string white34 = "";
+            string white35 = "";
+            string white36 = "";
+            string white37 = "";
+            string white38 = "";
+            string white39 = "";
+            string white40 = "";
+
+
+            string[] data = Console.ReadLine().Trim().Split();
+            int num1 = int.Parse(data[0]);
+            int num11 = int.Parse(data[1]);
+
+            string[] data2 = Console.ReadLine().Trim().Split();
+            int num2 = int.Parse(data2[0]);
+            int num22 = int.Parse(data2[1]);
+
+            string[] data3 = Console.ReadLine().Trim().Split();
+            int num3 = int.Parse(data3[0]);
+            int num33 = int.Parse(data3[1]);
+
+            string[] data4 = Console.ReadLine().Trim().Split();
+            int num4 = int.Parse(data4[0]);
+            int num44 = int.Parse(data4[1]);
+
+            string[] data5 = Console.ReadLine().Trim().Split();
+            int num5 = int.Parse(data5[0]);
+            int num55 = int.Parse(data5[1]);
+
+            string[] data6 = Console.ReadLine().Trim().Split();
+            int x = int.Parse(data6[0]);
+            int y = int.Parse(data6[1]);
+
+            int a = x + 2;
+            int a1 = y + 1;
+
+            int b = x + 2;
+            int b1 = y - 1;
+
+            int c = x - 2;
+            int c1 = y + 1;
+
+            int d = x - 2;
+            int d1 = y - 1;
+
+            int e = x + 1;
+            int e1 = y + 2;
+
+            int f = x - 1;
+            int f1 = y + 2;
+
+            int g = x + 1;
+            int g1 = y - 2;
+
+            int h = x - 1;
+            int h1 = y - 2;
+
+
+            if (num1 == a && num11 == a1)
+            {
+                white1 = Convert.ToString(a) + " " + Convert.ToString(a1) + " ";
+            }
+            if (num1 == b && num11 == b1)
+            {
+                white2 = Convert.ToString(b) + " " + Convert.ToString(b1) + " ";
+            }
+            if (num1 == c && num11 == c1)
+            {
+                white3 = Convert.ToString(c) + " " + Convert.ToString(c1) + " ";
+            }
+            if (num1 == d && num11 == d1)
+            {
+                white4 = Convert.ToString(d) + " " + Convert.ToString(d1) + " ";
+            }
+            if (num1 == e && num11 == e1)
+            {
+                white5 = Convert.ToString(e) + " " + Convert.ToString(e1) + " ";
+            }
+            if (num1 == f && num11 == f1)
+            {
+                white6 = Convert.ToString(f) + " " + Convert.ToString(f1) + " ";
+            }
+            if (num1 == g && num11 == g1)
+            {
+                white7 = Convert.ToString(g) + " " + Convert.ToString(g1) + " ";
+            }
+            if (num1 == h && num11 == h1)
+            {
+                white8 = Convert.ToString(h) + " " + Convert.ToString(h1) + " ";
+            }
+
+
+            if (num2 == a && num22 == a1)
+            {
+                white9 = Convert.ToString(a) + " " + Convert.ToString(a1) + " ";
+            }
+            if (num2 == b && num22 == b1)
+            {
+                white10 = Convert.ToString(b) + " " + Convert.ToString(b1) + " ";
+            }
+            if (num2 == c && num22 == c1)
+            {
+                white11 = Convert.ToString(c) + " " + Convert.ToString(c1) + " ";
+            }
+            if (num2 == d && num22 == d1)
+            {
+                white12 = Convert.ToString(d) + " " + Convert.ToString(d1) + " ";
+            }
+            if (num2 == e && num22 == e1)
+            {
+                white13 = Convert.ToString(e) + " " + Convert.ToString(e1) + " ";
+            }
+            if (num2 == f && num22 == f1)
+            {
+                white14 = Convert.ToString(f) + " " + Convert.ToString(f1) + " ";
+            }
+            if (num2 == g && num22 == g1)
+            {
+                white15 = Convert.ToString(g) + " " + Convert.ToString(g1) + " ";
+            }
+            if (num2 == h && num22 == h1)
+            {
+                white16 = Convert.ToString(h) + " " + Convert.ToString(h1) + " ";
+            }
+
+
+            if (num3 == a && num33 == a1)
+            {
+                white17 = Convert.ToString(a) + " " + Convert.ToString(a1) + " ";
+            }
+            if (num3 == b && num33 == b1)
+            {
+                white18 = Convert.ToString(b) + " " + Convert.ToString(b1) + " ";
+            }
+            if (num3 == c && num33 == c1)
+            {
+                white19 = Convert.ToString(c) + " " + Convert.ToString(c1) + " ";
+            }
+            if (num3 == d && num33 == d1)
+            {
+                white20 = Convert.ToString(d) + " " + Convert.ToString(d1) + " ";
+            }
+            if (num3 == e && num33 == e1)
+            {
+                white21 = Convert.ToString(e) + " " + Convert.ToString(e1) + " ";
+            }
+            if (num3 == f && num33 == f1)
+            {
+                white22 = Convert.ToString(f) + " " + Convert.ToString(f1) + " ";
+            }
+            if (num3 == g && num33 == g1)
+            {
+                white23 = Convert.ToString(g) + " " + Convert.ToString(g1) + " ";
+            }
+            if (num3 == h && num33 == h1)
+            {
+                white24 = Convert.ToString(h) + " " + Convert.ToString(h1) + " ";
+            }
+
+
+            if (num4 == a && num44 == a1)
+            {
+                white25 = Convert.ToString(a) + " " + Convert.ToString(a1) + " ";
+            }
+            if (num4 == b && num44 == b1)
+            {
+                white26 = Convert.ToString(b) + " " + Convert.ToString(b1) + " ";
+            }
+            if (num4 == c && num44 == c1)
+            {
+                white27 = Convert.ToString(c) + " " + Convert.ToString(c1) + " ";
+            }
+            if (num4 == d && num44 == d1)
+            {
+                white28 = Convert.ToString(d) + " " + Convert.ToString(d1) + " ";
+            }
+            if (num4 == e && num44 == e1)
+            {
+                white29 = Convert.ToString(e) + " " + Convert.ToString(e1) + " ";
+            }
+            if (num4 == f && num44 == f1)
+            {
+                white30 = Convert.ToString(f) + " " + Convert.ToString(f1) + " ";
+            }
+            if (num4 == g && num44 == g1)
+            {
+                white31 = Convert.ToString(g) + " " + Convert.ToString(g1) + " ";
+            }
+            if (num4 == h && num44 == h1)
+            {
+                white32 = Convert.ToString(h) + " " + Convert.ToString(h1) + " ";
+            }
+
+
+            if (num5 == a && num55 == a1)
+            {
+                white33 = Convert.ToString(a) + " " + Convert.ToString(a1) + " ";
+            }
+            if (num5 == b && num55 == b1)
+            {
+                white34 = Convert.ToString(b) + " " + Convert.ToString(b1) + " ";
+            }
+            if (num5 == c && num55 == c1)
+            {
+                white35 = Convert.ToString(c) + " " + Convert.ToString(c1) + " ";
+            }
+            if (num5 == d && num55 == d1)
+            {
+                white36 = Convert.ToString(d) + " " + Convert.ToString(d1) + " ";
+            }
+            if (num5 == e && num55 == e1)
+            {
+                white37 = Convert.ToString(e) + " " + Convert.ToString(e1) + " ";
+            }
+            if (num5 == f && num55 == f1)
+            {
+                white38 = Convert.ToString(f) + " " + Convert.ToString(f1) + " ";
+            }
+            if (num5 == g && num55 == g1)
+            {
+                white39 = Convert.ToString(g) + " " + Convert.ToString(g1) + " ";
+            }
+            if (num5 == h && num55 == h1)
+            {
+                white40 = Convert.ToString(h) + " " + Convert.ToString(h1) + " ";
+            }
+
+
+
+
+            white = white1 + white2 + white3 + white4 + white5 + white6 + white7 + white8 + white9 + white10 + white11 + white12
+            + white13 + white14 + white15 + white16 + white17 + white18 + white19 + white20 + white21 + white22 + white23
+             + white24 + white25 + white26 + white27 + white28 + white29 + white30 + white31 + white32 + white33 + white34
+              + white35 + white36 + white37 + white38 + white39 + white40;
+
+            Console.WriteLine(white);
+
+        }
+    }
+}

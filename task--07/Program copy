d56759ae71bb4a278e18ce70862afc5eb1416323
gladja <user@MacBook-Pro-user.local>
2023-07-29 09:09:17 +0300

    task--07

---
+++ task--07/Program copy
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace task_10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int a = num / 1000;
+            int b = (num / 100) % 10;
+            int c = (num / 10) % 10;
+            int d = num % 10;
+
+
+            if (b != 0 && a > b)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+                // Console.WriteLine("yes1");
+            }
+
+            if (b != 0 && b > c)
+            {
+                b = b + c;
+                c = b - c;
+                b = b - c;
+                // Console.WriteLine("yes2");
+
+            }
+
+            if (b != 0 && a > b)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+                // Console.WriteLine("yes3");
+
+            }
+
+            if (c > d)
+            {
+                c = c + d;
+                d = c - d;
+                c = c - d;
+            }
+
+            if (b > c)
+            {
+                b = b + c;
+                c = b - c;
+                b = b - c;
+            }
+
+            if (a > d)
+            {
+                a = a + d;
+                d = a - d;
+                a = a - d;
+            }
+
+            if (b != 0 && a > c)
+            {
+                a = a + c;
+                c = a - c;
+                a = a - c;
+            }
+
+            Console.WriteLine("{0}{1}{2}{3}", a, b, c, d);
+        }
+    }
+}
--- task--07/Program.cs
+++ task--07/Program.cs
@@ -13,62 +13,163 @@ namespace task_10
             int c = (num / 10) % 10;
             int d = num % 10;
 
-
-            if (b != 0 && a > b)
+            if (a != 0 && b != 0 && c != 0 && d != 0)
             {
-                a = a + b;
-                b = a - b;
-                a = a - b;
-                // Console.WriteLine("yes1");
-            }
+                if (a > b)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
 
-            if (b != 0 && b > c)
+                if (b > c)
+                {
+                    b = b + c;
+                    c = b - c;
+                    b = b - c;
+
+                }
+
+                if (a > b)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
+
+                if (c > d)
+                {
+                    c = c + d;
+                    d = c - d;
+                    c = c - d;
+                }
+
+                if (b > c)
+                {
+                    b = b + c;
+                    c = b - c;
+                    b = b - c;
+                }
+
+                if (a > b)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
+
+                Console.WriteLine("{0}{1}{2}{3}", a, b, c, d);
+            }
+            //d==0
+            if (a != 0 && b != 0 && c != 0 && d == 0)
             {
-                b = b + c;
-                c = b - c;
-                b = b - c;
-                // Console.WriteLine("yes2");
-
+                if (a > b)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
+                if (b > c)
+                {
+                    b = b + c;
+                    c = b - c;
+                    b = b - c;
+                }
+                if (a > b)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
+                Console.WriteLine("{0}{1}{2}{3}", a, d, b, c);
             }
-
-            if (b != 0 && a > b)
+            //c==0
+            if (a != 0 && b != 0 && c == 0 && d != 0)
             {
-                a = a + b;
-                b = a - b;
-                a = a - b;
-                // Console.WriteLine("yes3");
-
+                if (a > b)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
+                if (b > d)
+                {
+                    b = b + d;
+                    d = b - d;
+                    b = b - d;
+                }
+                if (a > b)
+                {
+                    a = a + b;
+                    b = a - b;
+                    a = a - b;
+                }
+                Console.WriteLine("{0}{1}{2}{3}", a, c, b, d);
             }
-
-            if (c > d)
+            //b==0
+            if (a != 0 && b == 0 && c != 0 && d != 0)
             {
-                c = c + d;
-                d = c - d;
-                c = c - d;
+                if (a > c)
+                {
+                    a = a + c;
+                    c = a - c;
+                    a = a - c;
+                }
+                if (c > d)
+                {
+                    c = c + d;
+                    d = c - d;
+                    c = c - d;
+                }
+                if (a > c)
+                {
+                    a = a + c;
+                    c = a - c;
+                    a = a - c;
+                }
+                Console.WriteLine("{0}{1}{2}{3}", a, b, c, d);
             }
-
-            if (b > c)
+            //c==0 d==0
+            if (a != 0 && b != 0 && c == 0 && d == 0)
             {
-                b = b + c;
-                c = b - c;
-                b = b - c;
+                if (a > b)
+                {
+                    Console.WriteLine("{0}{1}{2}{3}", b, c, d, a);
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}{2}{3}", a, c, d, b);
+                }
             }
-
-            if (a > d)
+            //b==0 d==0
+            if (a != 0 && b == 0 && c != 0 && d == 0)
             {
-                a = a + d;
-                d = a - d;
-                a = a - d;
+                if (a > c)
+                {
+                    Console.WriteLine("{0}{1}{2}{3}", c, b, d, a);
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}{2}{3}", a, b, d, c);
+                }
             }
-
-            if (b != 0 && a > c)
+            //b==0 c==0
+            if (a != 0 && b == 0 && c == 0 && d != 0)
             {
-                a = a + c;
-                c = a - c;
-                a = a - c;
+                if (a > d)
+                {
+                    Console.WriteLine("{0}{1}{2}{3}", d, b, c, a);
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}{2}{3}", a, b, c, d);
+                }
             }
-
-            Console.WriteLine("{0}{1}{2}{3}", a, b, c, d);
+            //b==0 c==0 d==0
+            if (b == 0 && c == 0 && d == 0)
+            {
+                Console.WriteLine("{0}{1}{2}{3}", a, b, c, d);
+            }
         }
     }
 }

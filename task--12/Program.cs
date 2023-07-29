5f256445daeda4cd79882f301256c200d027847d
gladja <user@MacBook-Pro-user.local>
2023-07-29 18:05:01 +0300

    task--12

---
+++ task--12/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace task_12
+{
+    class Program
+    {
+        static void Main()
+        {
+            // int num1 = Convert.ToInt32(Console.ReadLine());
+            double num = Convert.ToDouble(Console.ReadLine());
+            int num1 = (int)num;
+            // Console.WriteLine(num);
+            // Console.WriteLine(num1);
+            // Console.WriteLine(num - num1);
+            // if 
+            // double a = num / 1000;
+            // Console.WriteLine(a);
+            // double b = num % 1000 / 100;
+            // Console.WriteLine(b);
+            // double c = num % 100 / 10;
+            // Console.WriteLine(c);
+            // double d = num % 10;
+            // Console.WriteLine(d);
+
+
+            // double numTwo = Math.Round(num);
+            // double numTwo = ((num * 100) % 1000);
+            // Console.WriteLine(numTwo);
+            if (num - num1 == 0)
+            {
+                if (num > 0 && num <= 9)
+                {
+                    Console.WriteLine(num);
+                }
+                if (num < 0 && num >= -9)
+                {
+                    Console.WriteLine(num * (-1));
+                }
+                //2
+                if (num > 9 && num <= 99)
+                {
+                    int a = (int)num / 10;
+                    int b = (int)num % 10;
+                    Console.WriteLine(a + b);
+                }
+                if (num < -9 && num >= -99)
+                {
+                    int a = (int)num / 10;
+                    int b = (int)num % 10;
+                    Console.WriteLine((a + b) * (-1));
+                }
+                //3
+                if (num > 99 && num <= 999)
+                {
+                    int a = (int)num / 100;
+                    int b = ((int)num / 10) % 10;
+                    int c = (int)num % 10;
+                    Console.WriteLine(a + b + c);
+                }
+                if (num < -99 && num >= -999)
+                {
+                    int a = (int)num / 100;
+                    int b = ((int)num / 10) % 10;
+                    int c = (int)num % 10;
+                    Console.WriteLine((a + b + c) * (-1));
+                }
+                //4
+                if (num > 999 && num <= 9999)
+                {
+                    int a = (int)num / 1000;
+                    int b = ((int)num / 100) % 10;
+                    int c = ((int)num / 10) % 10;
+                    int d = (int)num % 10;
+                    Console.WriteLine(a + b + c + d);
+                }
+                if (num < -999 && num >= -9999)
+                {
+                    int a = (int)num / 1000;
+                    int b = ((int)num / 100) % 10;
+                    int c = ((int)num / 10) % 10;
+                    int d = (int)num % 10;
+                    Console.WriteLine((a + b + c + d) * (-1));
+                }
+                //5
+                if (num > 9999 && num <= 99999)
+                {
+                    int a = (int)num / 10000;
+                    int b = ((int)num / 1000) % 10;
+                    int c = ((int)num / 100) % 10;
+                    int d = ((int)num / 10) % 10;
+                    int e = (int)num % 10;
+                    Console.WriteLine(a + b + c + d + e);
+                }
+                if (num < -9999 && num >= -99999)
+                {
+                    int a = (int)num / 10000;
+                    int b = ((int)num / 1000) % 10;
+                    int c = ((int)num / 100) % 10;
+                    int d = ((int)num / 10) % 10;
+                    int e = (int)num % 10;
+                    Console.WriteLine((a + b + c + d + e) * (-1));
+                }
+            }
+            else
+            {
+                // double sum = ((num - num1) / 2) + num1;
+                Console.WriteLine(Math.Sqrt(num));
+                // Console.WriteLine("{0:F1}", sum);
+            }
+
+        }
+    }
+}
+
+
+
+

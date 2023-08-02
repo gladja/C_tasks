42ef3c5572e7959a63ea3d7aaccf1d4dcfe40264
gladja <gladja@yandex.ru>
2023-08-02 22:43:47 +0300

    task---01

---
+++ task---03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task_03
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            while (i <= num)
+            {
+                i += 1;
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
+
+
+
+

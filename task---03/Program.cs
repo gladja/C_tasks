b4e0c71dbc9c3eae539943547538149586dae758
gladja <user@MacBook-Pro-user.local>
2023-08-03 00:04:29 +0300

    task---02

--- task---03/Program.cs
+++ task---03/Program.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 
-namespace task_03
+namespace task_01
 {
     class Program
     {
         static void Main()
         {
             int num = Convert.ToInt32(Console.ReadLine());
-            int i = 1;
-            while (i <= num)
+            int count = 0;
+
+            while (num > 0)
             {
-                i += 1;
-                Console.WriteLine("Hello!");
+                int rez = num % 2;
+                num /= 2;
+                if (rez > 0)
+                {
+                    count += 1;
+                }
             }
+            Console.WriteLine(count);
+
+
         }
     }
 }

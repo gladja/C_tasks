764645fe28fa423fd311a121bbdc6cfed5711a12
gladja <gladja@MacBook-Pro-gladja.local>
2023-08-04 17:20:27 +0300

    task06

--- task---03/Program.cs
+++ task---03/Program.cs
@@ -6,20 +6,37 @@ namespace task_01
     {
         static void Main()
         {
-            int num = Convert.ToInt32(Console.ReadLine());
-            int count = 0;
-            int sum = num;
+            double num = Convert.ToDouble(Console.ReadLine());
+            int count = 1;
+            int count2 = 0;
+            double sum = 1;
 
-            while (num > 0 || num < 0)
+            // while (sum < num)
+            // {
+            //     sum += 1.0 / (count + 1);
+            //     count++;
+            //     Console.WriteLine(sum);
+
+            // }
+
+
+            for (int i = 2; sum > num; i++)
             {
-                num = Convert.ToInt32(Console.ReadLine());
-                if (sum < num)
-                {
-                    count += 1;
-                }
-                sum = num;
+                sum += 1.0 / i;
+                count++;
+                // Console.WriteLine(i);
+                Console.WriteLine(sum);
             }
             Console.WriteLine(count);
+
+            // for (int i = 2; sum < num; i++)
+            // {
+            //     sum += 1.0 / i;
+            //     count++;
+            //     // Console.WriteLine(i);
+            //     Console.WriteLine(sum);
+            // }
+            // Console.WriteLine(count2);
         }
     }
 }

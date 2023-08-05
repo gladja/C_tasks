eef08d0db015853e9e60d811b16a16d5d6d5349c
gladja <gladja@MacBook-Pro-gladja.local>
2023-08-05 23:51:07 +0300

    task23

--- task---03/Program.cs
+++ task---03/Program.cs
@@ -6,17 +6,48 @@ namespace task_01
     {
         static void Main()
         {
-            double n = double.Parse(Console.ReadLine());
-            double sum = 1.1;
-            double x = 1.1;
+            string[] data = Console.ReadLine().Trim().Split();
+            int a = int.Parse(data[0]);
+            int b = int.Parse(data[1]);
 
-            for (int i = 2; i <= n; i++)
+            int n = int.Parse(Console.ReadLine());
+            int count = 1;
+
+            int countH = 0;
+            Console.WriteLine($" {a.ToString("00")}:{b.ToString("00")}");
+            while (n > count)
             {
-                x += 0.1;
-                sum *= x;
-                // Console.WriteLine(x);
+                count++;
+                for (int i = 1; i <= 5; i++)
+                {
+                    if (b % 59 == 0 && b != 0)
+                    {
+                        b = 0;
+                        countH++;
+                        // Console.WriteLine(b);
+                    }
+                    else
+                    {
+                        b += 1;
+                        // Console.WriteLine(b);
+                    }
+                }
+
+                if (countH > 0)
+                {
+                    if (a % 23 == 0 && a != 0)
+                    {
+                        a = 0;
+                    }
+                    else
+                    {
+                        a++;
+                    }
+                }
+                countH = 0;
+                // Console.WriteLine();
+                Console.WriteLine($" {a.ToString("00")}:{b.ToString("00")}");
             }
-            Console.WriteLine($"{sum:F6}");
         }
     }
 }

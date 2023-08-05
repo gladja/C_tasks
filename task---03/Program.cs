6529fd1fb1c3911c7bc3696a5449f60ed2c3919a
gladja <gladja@MacBook-Pro-gladja.local>
2023-08-05 15:13:57 +0300

    task19

--- task---03/Program.cs
+++ task---03/Program.cs
@@ -6,13 +6,18 @@ namespace task_01
     {
         static void Main()
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            int k = Convert.ToInt32(Console.ReadLine());
+            string[] data = Console.ReadLine().Trim().Split();
+            int a = int.Parse(data[0]);
+            int b = int.Parse(data[1]);
+            int i = 10;
+            a *= 100;
 
-            for (int i = 0; i < k; i++)
-            {
-                Console.WriteLine(n);
-            }
+            a *= i;
+            a /= 100;
+            b *= i;
+            // int sum = (a + b) * i;
+            Console.WriteLine("{0},{1}", a, b);
+
         }
     }
 }

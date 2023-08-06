f0d732bdc6c293e1ee78a11e48ea546d5fcdd8b4
gladja <user@MacBook-Pro-user.local>
2023-08-06 22:04:13 +0300

    task28

--- task---03/Program.cs
+++ task---03/Program.cs
@@ -7,21 +7,37 @@ namespace task_01
         static void Main()
         {
             int k = int.Parse(Console.ReadLine());
-            int sum = k;
-            int sum2 = k;
+            int sum = 1000;
+            int sum2 = 0;
 
-            while (sum % k == 0 && sum < 10000)
+            while (sum < 10000)
             {
-
-                sum += k;
-                if (sum < 10000 && sum > 999)
+                if (sum % k == 0)
                 {
                     sum2 += sum;
-                    Console.WriteLine(sum);
+                    // Console.WriteLine(sum);
                 }
+                sum++;
                 // Console.WriteLine(sum);
             }
+
             Console.WriteLine(sum2);
         }
     }
 }
+
+// while (sum % k == 0 && sum < 10000)
+//             {
+//                 sum += k;
+//                 if (sum < 10000 && sum >= 999)
+//                 {
+//                     sum2 += sum;
+//                     Console.WriteLine(sum);
+//                 }
+//                 // Console.WriteLine(sum);
+//             }
+//             if (k > 999)
+//             {
+//                 sum2 += k;
+//             }
+//             Console.WriteLine(sum2);

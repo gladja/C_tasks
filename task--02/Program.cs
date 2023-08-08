78d9424bdfc86a19dda8ad08dd1c79880b903189
gladja <user@MacBook-Pro-user.local>
2023-08-08 20:03:22 +0300

    refactor

--- task--02/Program.cs
+++ task--02/Program.cs
@@ -1,113 +1,27 @@
 ﻿using System;
 
-namespace task_02
+namespace task_01
 {
     class Program
     {
         static void Main()
         {
-            string white = "";
-            string black = "";
-            string white1 = "";
-            string black1 = "";
-            string white2 = "";
-            string black2 = "";
-            string white3 = "";
-            string black3 = "";
-            string white4 = "";
-            string black4 = "";
-            string white5 = "";
-            string black5 = "";
-            string white6 = "";
-            string black6 = "";
+            int num = Convert.ToInt32(Console.ReadLine());
 
-
-            string[] data = Console.ReadLine().Trim().Split();
-            int a = int.Parse(data[0]);
-            int b = int.Parse(data[1]);
-
-            string[] data2 = Console.ReadLine().Trim().Split();
-            int c = int.Parse(data2[0]);
-            int d = int.Parse(data2[1]);
-
-            string[] data3 = Console.ReadLine().Trim().Split();
-            int e = int.Parse(data3[0]);
-            int f = int.Parse(data3[1]);
-
-            string[] data4 = Console.ReadLine().Trim().Split();
-            int g = int.Parse(data4[0]);
-            int h = int.Parse(data4[1]);
-
-            string[] data5 = Console.ReadLine().Trim().Split();
-            int i = int.Parse(data5[0]);
-            int j = int.Parse(data5[1]);
-
-            string[] data6 = Console.ReadLine().Trim().Split();
-            int k = int.Parse(data6[0]);
-            int l = int.Parse(data6[1]);
-
-
-
-            if ((a + b) % 2 == 0)
+            if (num == 0)
             {
-                black1 = Convert.ToString(a) + " " + Convert.ToString(b) + " ";
+                Console.WriteLine('0');
+            }
+            else if (num > 0)
+            {
+                num = num * num;
+                Console.WriteLine(num);
             }
             else
             {
-                white1 = Convert.ToString(a) + " " + Convert.ToString(b) + " ";
+                num *= -1;
+                Console.WriteLine(num);
             }
-
-            if ((c + d) % 2 == 0)
-            {
-                black2 = Convert.ToString(c) + " " + Convert.ToString(d) + " ";
-            }
-            else
-            {
-                white2 = Convert.ToString(c) + " " + Convert.ToString(d) + " ";
-            }
-
-            if ((e + f) % 2 == 0)
-            {
-                black3 = Convert.ToString(e) + " " + Convert.ToString(f) + " ";
-            }
-            else
-            {
-                white3 = Convert.ToString(e) + " " + Convert.ToString(f) + " ";
-            }
-
-            if ((g + h) % 2 == 0)
-            {
-                black4 = Convert.ToString(g) + " " + Convert.ToString(h) + " ";
-            }
-            else
-            {
-                white4 = Convert.ToString(g) + " " + Convert.ToString(h) + " ";
-            }
-
-            if ((i + j) % 2 == 0)
-            {
-                black5 = Convert.ToString(i) + " " + Convert.ToString(j) + " ";
-            }
-            else
-            {
-                white5 = Convert.ToString(i) + " " + Convert.ToString(j) + " ";
-            }
-
-            if ((k + l) % 2 == 0)
-            {
-                black6 = Convert.ToString(k) + " " + Convert.ToString(l) + " ";
-            }
-            else
-            {
-                white6 = Convert.ToString(k) + " " + Convert.ToString(l) + " ";
-            }
-
-            white = white1 + white2 + white3 + white4 + white5 + white6;
-            black = black1 + black2 + black3 + black4 + black5 + black6;
-
-            Console.WriteLine(white);
-            Console.WriteLine(black);
-
         }
     }
 }

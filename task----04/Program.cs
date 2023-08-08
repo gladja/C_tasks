a00d138c7f2f9dfa1b5d4aa5134c7679239865c9
gladja <user@MacBook-Pro-user.local>
2023-08-08 21:49:20 +0300

    task---04

--- task----04/Program.cs
+++ task----04/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace task_01
+{
+    class Program
+    {
+        static void PrintAllFracs(int num)
+        {
+            double sum = 0;
+            int count = 0;
+
+            for (double i = 1; i <= num; i++)
+            {
+                count++;
+                for (double j = 0; j <= num; j++)
+                {
+                    sum = i / j;
+                    if (sum > 0 && sum < 1)
+                    {
+                        if (j == count)
+                        {
+
+                        }
+                        Console.WriteLine($"{i}/{j}");
+                    }
+                }
+            }
+        }
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            PrintAllFracs(num);
+        }
+    }
+}
+
+
+

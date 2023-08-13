f1280f5739371ecc29cc2ccd3e27ae20114f49b4
gladja <gladja@MacBook-Pro-gladja.local>
2023-08-13 09:41:20 +0300

    task02

--- task----04/Program.cs
+++ task----04/Program.cs
@@ -16,9 +16,43 @@ namespace Hello
             return arr;
         }
 
-        static void CountOfDifferent(int[] arr)
+        static void SumMin(int[] arr)
         {
-            int max = 0;
+            int min = int.MaxValue;
+            int firstMin = arr[0]; ;
+            int value = 0;
+
+
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string res = Math.Abs(arr[i]).ToString();
+                for (int j = 0; j < res.Length; j++)
+                {
+                    int b = int.Parse($"{res[j]}");
+                    value += b;
+                }
+                if (value <= min)
+                {
+                    min = value;
+                    // Console.WriteLine(min);
+                    if (firstMin > arr[i])
+                    {
+                        firstMin = arr[i];
+                        count = i;
+                    }
+                }
+                value = 0;
+            }
+            Console.WriteLine(arr[count]);
+            // Console.WriteLine(min);
+            // Console.WriteLine(max);
+            // return max;
+        }
+
+        static void SumMax(int[] arr)
+        {
+            int max = int.MinValue;
             int firstMax = arr[0]; ;
             int value = 0;
 
@@ -26,7 +60,7 @@ namespace Hello
             int count = 0;
             for (int i = 0; i < arr.Length; i++)
             {
-                string res = arr[i].ToString();
+                string res = Math.Abs(arr[i]).ToString();
                 for (int j = 0; j < res.Length; j++)
                 {
                     int b = int.Parse($"{res[j]}");
@@ -35,20 +69,15 @@ namespace Hello
                 if (value >= max)
                 {
                     max = value;
-                    // Console.WriteLine(max);
                     if (firstMax < arr[i])
                     {
                         firstMax = arr[i];
                         count = i;
                     }
                 }
-
                 value = 0;
             }
             Console.WriteLine(arr[count]);
-            // Console.WriteLine(min);
-            // Console.WriteLine(max);
-            // return max;
         }
 
 
@@ -58,7 +87,8 @@ namespace Hello
             int n = Convert.ToInt16(Console.ReadLine());
             int[] myArr = InitByKeyBoard(Console.ReadLine());
 
-            CountOfDifferent(myArr);
+            SumMin(myArr);
+            SumMax(myArr);
         }
     }
 }

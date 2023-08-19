7bc13928f4d67f3a9cf95730df32aa782e5c7591
gladja <gladja@MacBook-Pro-gladja.local>
2023-08-19 08:38:10 +0300

    task04

--- task-05/Program.cs
+++ task-05/Program.cs
@@ -5,44 +5,72 @@ namespace Hello
     class Program
     {
 
-        static int[] InitByKeyBoard(string str)
+        static int[] InitByKeyBoard(int n)
         {
-            string[] date = str.Trim().Split();
-            int[] arr = new int[date.Length];
-            for (int i = 0; i < arr.Length; i++)
+            int[] arr = new int[n + n];
+            int count = 2;
+
+            for (int i = 1; i <= n; i++)
             {
-                arr[i] = Int32.Parse(date[i]);
+                arr[i] = (int)Math.Pow(i, 2);
+                // Console.WriteLine($"one {arr[i]}");
+            }
+            for (int i = n + 1; i < arr.Length; i++)
+            {
+                arr[i] = (int)Math.Pow(count, 3);
+                // Console.WriteLine($"two {arr[i]}");
+                count++;
             }
             return arr;
         }
 
-
-        static int[] CountOfDifferent(int[] arr, int b)
+        static int[] CountOfDifferent(int[] arr)
         {
-            for (int i = b - 1; i < arr.Length - 1; i++)
+
+            for (int i = 0; i < arr.Length - 1; i++)
             {
-                arr[i] = arr[i + 1];
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j + 1] < arr[j])
+                    {
+                        int t = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = t;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        static void Created(int[] arr, int n)
+        {
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    arr[i + 1] = arr[i + 2];
+                }
             }
             Array.Resize(ref arr, arr.Length - 1);
-            return arr;
-        }
-
-        static void Created(int[] arr)
-        {
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.Write($"{arr[i]} ");
+            }
+            if (n == 1)
+            {
+                Console.WriteLine($"1");
+            }
+            else
+            {
+                // Console.WriteLine($"{arr[n]} ");
             }
         }
 
         static void Main(string[] args)
         {
             int n = Convert.ToInt16(Console.ReadLine());
-            int[] myArr = InitByKeyBoard(Console.ReadLine());
-            int b = Convert.ToInt16(Console.ReadLine());
 
-            Created(CountOfDifferent(myArr, b));
-
+            Created(CountOfDifferent(InitByKeyBoard(n)), n);
         }
     }
 }

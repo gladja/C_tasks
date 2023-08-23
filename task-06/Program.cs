f0844d6e765875d6a54a5125927d0cc42e82c77e
gladja <gladkih.alexander@gmail.com>
2023-08-23 21:50:09 +0300

    rafactor

--- task-06/Program.cs
+++ task-06/Program.cs
@@ -3,50 +3,61 @@
 public class Test
 {
 
-    static int[,] InitByKeyBoardLine(int n, int m)
+    static int[,] InitByKeyBoardLine(int n)
     {
-        int[,] arr = new int[n, m];
-        string[] str_arr;
+        int[,] arr = new int[n, n];
+        // int m = n;
+        int count = 0;
+
         for (int i = 0; i < n; i++)
         {
-            str_arr = (Console.ReadLine()).Split(' ');
-            for (int j = 0; j < m; j++)
+            for (int j = 0; j < n - 1; j++)
             {
-                arr[i, j] = Convert.ToInt32(str_arr[j]);
+                arr[i, j] = 0;
             }
         }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (!(i == 0 || i == n - 1 || j == 0 || j == n - 1))
+                {
+
+                }
+                continue;      // Временное условие для фильтрации элементов внесшего "кольца"
+                int a = i + 1;     // Номера строк и столбцов приводим в удобный
+                int b = j + 1;     // в математическом плане вид (от 1 до N)  
+                                   //  ... здесь будем вставлять основной код вычислений
+            }
+        }
+
+
         return arr;
     }
 
-    static void NewArr(int[,] arr, int n, int m)
+    static void NewArr(int[,] arr)
     {
 
-        int sum = 0;
-        int count = 0;
-        double result = 0;
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < arr.GetLength(0); i++)
         {
-            for (int j = 0; j < m; j++)
+            for (int j = 0; j < arr.GetLength(1); j++)
             {
-                if (i > j)
-                {
-                    sum += arr[i, j];
-                    count++;
-                }
+                Console.Write($"{arr[i, j]} ");
             }
+            Console.WriteLine();
         }
-        result = (double)sum / count;
-        Console.Write($"{result:F6}");
     }
 
     static void Main(string[] args)
 
     {
-        string[] data = Console.ReadLine().Trim().Split();
-        int n = int.Parse(data[0]);
-        int m = int.Parse(data[1]);
+        // string[] data = Console.ReadLine().Trim().Split();
+        // int n = int.Parse(data[0]);
+        // int m = int.Parse(data[1]);
+        int n = Convert.ToInt16(Console.ReadLine());
 
-        NewArr(InitByKeyBoardLine(n, m), n, m);
+        NewArr(InitByKeyBoardLine(n));
     }
-
 }
+
